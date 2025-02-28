759acf136fbdf89b2ff100dd90e2044840d1e5d2
Antoine Massias <massias.antoine.pro@gmail.com>
2025-02-28 07:39:28 +0100

    Made global logic.

--- src/avaj/launcher/Main.java
+++ src/avaj/launcher/Main.java
@@ -1,9 +1,16 @@
 package avaj.launcher;
 
+import avaj.launcher.simulation.Simulation;
+import avaj.launcher.simulation.ScenarioParser;
+
 public class Main
 {
 	public static void main(String[] args) throws Exception
 	{
-		System.out.println("Hello, World!");
+		final ScenarioParser parser = new ScenarioParser("subject/scenario.txt");
+		final Simulation simulation = new Simulation(parser.getFlyables());
+
+		for (int i = 0; i < parser.getSteps(); ++i)
+			simulation.step();
 	}
 }
